52919faf4602b023b0ace8c2e86b25764db8f6e6
PatienceNdlovu <47353642+PatienceNdlovu@users.noreply.github.com>
2021-03-01 20:57:18 +0000

    basic calculator exercise
    created a basic addition calculator that adds two numbers

---
+++ freecodecamppythonProject/calculator_exercise.py
@@ -0,0 +1,7 @@
+"""Challenge is to create a basic calculator that will add any two numbers together incl decimals"""
+
+num1 = input("Enter your first number: ")
+num2 = input("Enter your second number: ")
+result = float(num1) + float(num2)
+
+print(result)
