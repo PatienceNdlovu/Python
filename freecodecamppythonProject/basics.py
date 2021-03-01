ac104a5313d5fc9eaa16fbe62573c354bfda4326
PatienceNdlovu <47353642+PatienceNdlovu@users.noreply.github.com>
2021-03-01 08:57:14 +0000

    Initial commit

---
+++ freecodecamppythonProject/basics.py
@@ -0,0 +1,31 @@
+print("Hello World")
+
+"""basic triangle"""
+print("    /|")
+print("   / |")
+print("  /  |")
+print(" /_ _|")
+
+"""This is how i do a new line in python can also use \" to do the same"""
+print("Patience\n Ndlovu")
+
+""" Functions"""
+phrase = "Python is cool"
+
+"""changes the variable to lowercase"""
+print(phrase.lower())
+
+"""changes the variable to uppercase and checks that its in uppercase"""
+print(phrase.upper().isupper())
+
+"""will tell you how many characters in the phrase"""
+print(len(phrase))
+
+"""used to find out what character is at the said index position"""
+print(phrase[4])
+
+"""This is called passing a parameter and it will give you the index number"""
+print(phrase.index("c"))
+
+"""replace function is used to change the statement"""
+print(phrase.replace("Python", "Coding"))
